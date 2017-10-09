d0581018dce1f67f84dcb470ec8b6f623f0c5e66
ciceromoraesgrangeiro <32551312+ciceromoraesgrangeiro@users.noreply.github.com>
2017-10-09 14:47:54 -0300

    Calculadora.java
    Teste, estudo

---
+++ .gitignore/Calculadora.java
@@ -0,0 +1,21 @@
+package calculadora;
+public class Calculadora{
+    
+    public void somaDoisNumeros(){
+      System.out.println(5+5);
+    }
+    
+    public void subraiDoisNumeros(){
+      System.out.println(5-5);
+    }
+    
+    public void multiplicaDoisNumeros(double num1, int num2){
+      System.out.println(num1*num2);
+    }
+    
+    public double divide Dois Numeros(double num1, double num2){
+    // System.out.println(num1*num2);
+       return num1/num2;
+    }
+    
+}
