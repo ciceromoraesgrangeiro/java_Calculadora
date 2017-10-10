e82c3fcb325d46a8f69df8386b07131858a0a45d
ciceromoraesgrangeiro <32551312+ciceromoraesgrangeiro@users.noreply.github.com>
2017-10-10 16:38:07 -0300

    Update Calculadora.java

--- .gitignore/Calculadora.java
+++ .gitignore/Calculadora.java
@@ -14,8 +14,12 @@ public class Calculadora{
     }
     
     public double divide Dois Numeros(double num1, double num2){
-    // System.out.println(num1*num2);
-       return num1/num2;
+    // System.out.println(num1/num2);
+        if(num2 !=0){
+            System.out.println(num1/num2);
+            return;
+        }
+        System.out.println("Nao e possivel dividir por 0");
     }
     
 }
